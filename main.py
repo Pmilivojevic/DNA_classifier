66fa2d26edc8fb5fb0d90cc6b736384e6cb25b8f
Petar Milivojevic <petar.milivojevic.biz@gmail.com>
2024-03-27 22:06:15 +0100

    data validation added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from dnaseq import logger
 from dnaseq.pipelines.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from dnaseq.pipelines.stage_02_data_validation import DataValidationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion"
@@ -8,6 +9,19 @@ try:
     logger.info(f">>>>>>>>>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<<<<<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(f">>>>>>>>>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<<<<<<<<")
+    
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Validation"
+
+try:
+    logger.info(f">>>>>>>>>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<<<<<<<<")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
     logger.info(f">>>>>>>>>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<<<<<<<<")
     
 except Exception as e:
--- src/dnaseq/entity/config_entity.py
+++ src/dnaseq/entity/config_entity.py
@@ -7,4 +7,11 @@ class DataIngestionConfig:
     root_dir: Path
     source_URL: str
     local_data_file: Path
-    
+
+
+@dataclass(frozen=True)
+class DataValidationConfig:
+    root_dir: Path
+    dwn_data_file: Path
+    STATUS_FILE: Path
+    all_schema: dict
